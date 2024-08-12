2ff58b8cdb5c5916c5535206e87162bd54889a7c
Talpx1 <simo3401@gmail.com>
2024-08-12 22:50:45 +0200

    added simple modals for privacy and contacts

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
 import { APP_TITLE } from '@/main'
-import { RouterLink } from 'vue-router'
+import { useToggle } from '@vueuse/core'
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { PrimeIcons } from '@primevue/core/api'
+
+const { t } = useI18n()
+
+const isPrivacyDialogOpen = ref(false)
+const isContactDialogOpen = ref(false)
+
+const togglePrivacyDialog = useToggle(isPrivacyDialogOpen)
+const toggleContactDialog = useToggle(isContactDialogOpen)
 </script>
 
 <template>
@@ -8,8 +21,19 @@ import { RouterLink } from 'vue-router'
         &copy;{{ new Date().getFullYear() }} {{ APP_TITLE }}
 
         <div class="flex gap-8">
-            <RouterLink :to="{ name: '' }">Privacy</RouterLink>
-            <RouterLink :to="{ name: '' }">Contattaci</RouterLink>
+            <Button text label="Privacy" @click="togglePrivacyDialog()" />
+            <Button text :label="t('contact_us')" @click="toggleContactDialog()" />
         </div>
+
+        <Dialog v-model:visible="isPrivacyDialogOpen" header="Privacy">
+            {{ t('privacy_disclaimer') }}
+        </Dialog>
+
+        <Dialog v-model:visible="isContactDialogOpen" :header="t('contact_us')">
+            <a href="mailto:software@simonecerruti.com" class="flex items-center gap-4">
+                <span :class="PrimeIcons.ENVELOPE"></span>
+                software@simonecerruti.com
+            </a>
+        </Dialog>
     </footer>
 </template>
