9737e882c5138dfaa3473227aabc7e90325e33e8
Talpx1 <simo3401@gmail.com>
2024-08-13 12:40:51 +0200

    added sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnheadVite from '@unhead/addons/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), UnheadVite()],
+    plugins: [
+        vue(),
+        UnheadVite(),
+        Sitemap({
+            dynamicRoutes: [
+                '/it',
+                '/en',
+                '/it/indice-massa-corporea-calcolatore',
+                '/en/body-mass-index-calculator'
+            ]
+        })
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
