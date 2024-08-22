7995bebb5beb302764b43c25dfcc88ebdcf1759e
Talpx1 <simo3401@gmail.com>
2024-08-22 23:23:12 +0200

    fixed blank starting line in sitemap and robots

--- scripts/robots_generator.ts
+++ scripts/robots_generator.ts
@@ -4,8 +4,7 @@ const SAVE_PATH = './public/robots.txt'
 const PRODUCTION_URL = 'https://coolculators.com'
 const SITEMAP_URL = `${PRODUCTION_URL}/sitemap.xml`
 
-const content = `
-User-agent: *
+const content = `User-agent: *
 Allow: /
 
 Sitemap: ${SITEMAP_URL}
